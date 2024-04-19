78b6e534f0d9fcafb344a3c4da6558c21c413850
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-20 00:13:24 +0200

    removed unused stuff

--- wda-rws/src/main/java/ch/hslu/informatik/swde/rws/reader/WdaProxy.java
+++ wda-rws/src/main/java/ch/hslu/informatik/swde/rws/reader/WdaProxy.java
@@ -1,6 +1,6 @@
-package ch.hslu.swde.wda.reader;
+package ch.hslu.informatik.swde.rws.reader;
 
-import ch.hslu.swde.wda.domain.*;
+import ch.hslu.informatik.swde.domain.*;
 
 import java.util.List;
 
@@ -15,40 +15,9 @@ import java.util.List;
 public interface WdaProxy {
 
     /**
-     * Liest eine Liste von Wetterstationen von einem externen Webservice.
-     *
-     * @return Eine Liste von Station-Objekten, die aus der Antwort des Webservice extrahiert wurden.
-     */
-    List<Station> readStation();
-
-    /**
      * Liest eine Liste von Ortschaften von einem externen Webservice.
      *
      * @return Eine Liste von Ortschaft-Objekten, die aus der Antwort des Webservice extrahiert wurden.
      */
-    List<Ortschaft> readOrtschaft();
-
-    /**
-     * Liest Wetterdaten für eine spezifische Ortschaft von einem externen Webservice.
-     *
-     * @param name Der Name der Ortschaft.
-     * @return Ein Wetter-Objekt mit den Wetterdaten der angegebenen Ortschaft.
-     */
-    Wetter readWetter(String name);
-
-    /**
-     * Liest alle Wetterdaten von einem externen Webservice.
-     *
-     * @return Eine Liste von Wetter-Objekten, die alle Wetterdaten enthalten.
-     */
-    List<Wetter> readAllWetter();
-
-    /**
-     * Liest Wetterdaten für eine spezifische Ortschaft und ein bestimmtes Jahr von einem externen Webservice.
-     *
-     * @param ortschaft Der Name der Ortschaft.
-     * @param jahr Das spezifizierte Jahr.
-     * @return Eine Liste von Wetter-Objekten, die den Wetterdaten für das angegebene Jahr und die Ortschaft entsprechen.
-     */
-    List<Wetter> readWetterJahr(String ortschaft, int jahr);
+    List<City> readOrtschaft();
 }
