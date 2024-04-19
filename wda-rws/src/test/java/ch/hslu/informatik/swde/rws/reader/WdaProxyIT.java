39cce799e822e90b0edd3686ad2ff6f843992402
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-20 00:39:14 +0200

    removed unused stuff

--- wda-rws/src/test/java/ch/hslu/informatik/swde/rws/reader/WdaProxyIT.java
+++ wda-rws/src/test/java/ch/hslu/informatik/swde/rws/reader/WdaProxyIT.java
@@ -1,11 +1,9 @@
-package ch.hslu.swde.wda.reader;
+package ch.hslu.informatik.swde.rws.reader;
 
-import ch.hslu.swde.wda.domain.Ortschaft;
-import ch.hslu.swde.wda.domain.Station;
-import ch.hslu.swde.wda.domain.Wetter;
-import ch.hslu.swde.wda.persister.DAO.OrtschaftDAO;
-import ch.hslu.swde.wda.persister.impl.OrtschaftDAOImpl;
-import ch.hslu.swde.wda.reader.util.Util;
+import ch.hslu.informatik.swde.domain.City;
+import ch.hslu.informatik.swde.persister.DAO.CityDAO;
+import ch.hslu.informatik.swde.persister.impl.CityDAOImpl;
+import ch.hslu.informatik.swde.rws.util.Util;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,65 +29,8 @@ class WdaProxyIT {
 
         WdaProxy proxy = new WdaProxyImpl();
 
-        List<Ortschaft> resOrt = proxy.readOrtschaft();
+        List<City> resOrt = proxy.readOrtschaft();
         assertNotNull(resOrt);
         assertEquals(40, resOrt.size());
-    }
-
-    @Test
-    void getStationen() {
-
-        WdaProxy proxy = new WdaProxyImpl();
-        Util.createOrtschaften();
-
-        List<Station> resStation = proxy.readStation();
-        assertNotNull(resStation);
-        assertEquals(40, resStation.size());
-    }
-
-    @Test
-    void getWetter() {
-
-        WdaProxy proxy = new WdaProxyImpl();
-        OrtschaftDAO daoOrtschaft = new OrtschaftDAOImpl();
-        Util.createOrtschaften();
-
-        List<Ortschaft> ortList = daoOrtschaft.alle();
-
-        Wetter resWetter = proxy.readWetter("Davos");
-        assertNotNull(resWetter);
-
-        int ortId_wetter = resWetter.getOrtschaftId();
-        int ortId_ortschaft = 0;
-        for (Ortschaft ortschaft : ortList) {
-            if (ortschaft.getName().equals("Davos")) {
-                ortId_ortschaft = ortschaft.getId();
-            }
-        }
-        assertEquals(ortId_ortschaft, ortId_wetter);
-    }
-
-    @Test
-    void getWetterJahr() {
-
-        WdaProxy proxy = new WdaProxyImpl();
-        OrtschaftDAO daoOrtschaft = new OrtschaftDAOImpl();
-        Util.createOrtschaften();
-
-        List<Wetter> resWetter = proxy.readWetterJahr("Davos", 2023);
-        assertNotNull(resWetter);
-
-        List<Ortschaft> ortList = daoOrtschaft.alle();
-        int ortId_ortschaft = 0;
-        for (Ortschaft ortschaft : ortList) {
-            if (ortschaft.getName().equals("Davos")) {
-                ortId_ortschaft = ortschaft.getId();
-            }
-        }
-
-        for (Wetter wetter : resWetter) {
-            int ortId_wetter = wetter.getOrtschaftId();
-            assertEquals(ortId_ortschaft, ortId_wetter);
-        }
     }
 }
