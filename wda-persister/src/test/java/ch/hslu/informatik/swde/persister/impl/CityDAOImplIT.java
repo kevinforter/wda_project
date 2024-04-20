3fed3661e745b03936da1f1a7045bf7de4609057
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-20 02:40:28 +0200

    made new test case

---
+++ wda-persister/src/test/java/ch/hslu/informatik/swde/persister/impl/CityDAOImplIT.java
@@ -0,0 +1,76 @@
+package ch.hslu.informatik.swde.persister.impl;
+
+import ch.hslu.informatik.swde.domain.City;
+import ch.hslu.informatik.swde.persister.DAO.CityDAO;
+import ch.hslu.informatik.swde.persister.impl.util.Util;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CityDAOImplIT {
+
+    @BeforeEach
+    void setUp() {
+        Util.cleanDatabase();
+    }
+
+    @AfterAll
+    static void clearUp() {
+        Util.cleanDatabase();
+    }
+
+    @Test
+    void testOrtschaftSpeichern() {
+
+        CityDAO dao = new CityDAOImpl();
+
+        for (City c : Util.createOrtschaftList()) {
+            dao.speichern(c);
+            assertEquals(c, dao.findById(c.getId()));
+        }
+
+    }
+
+    @Test
+    void testFindOrtschaftById() {
+
+        CityDAO dao = new CityDAOImpl();
+
+        for (City c : Util.createOrtschaftList()) {
+            dao.speichern(c);
+            assertEquals(c, dao.findById(c.getId()));
+        }
+    }
+
+    @Test
+    void testOrtschaftAlle() {
+
+        CityDAO dao = new CityDAOImpl();
+
+        List<City> listFromUtil = Util.createOrtschaftList();
+        List<City> listFromDB;
+
+        for (City c : listFromUtil) {
+            dao.speichern(c);
+            assertEquals(c, dao.findById(c.getId()));
+        }
+
+        listFromDB = dao.alle();
+        assertEquals(listFromUtil.size(), listFromDB.size());
+    }
+
+    @Test
+    void testFindCityByName() {
+
+        CityDAO dao = new CityDAOImpl();
+
+        for (City c : Util.createOrtschaftList()) {
+            dao.speichern(c);
+            assertEquals(c, dao.findCityByName(c.getName()));
+        }
+    }
+}
