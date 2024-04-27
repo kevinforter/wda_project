5a398725e0932d3b943b7867e521a6bec4555ad6
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-27 16:51:37 +0200

    added impl consturct

---
+++ wda-business/src/main/java/ch/hslu/informatik/swde/business/BusinessImpl.java
@@ -0,0 +1,33 @@
+package ch.hslu.informatik.swde.business;
+
+public class BusinessImpl implements BusinessAPI{
+    @Override
+    public void addAllCities() {
+
+    }
+
+    @Override
+    public void addCurrentWeatherOfCity() {
+
+    }
+
+    @Override
+    public void addWeatherOfCityByYear() {
+
+    }
+
+    @Override
+    public void getAllCities() {
+
+    }
+
+    @Override
+    public void getCurrentWeatherOfCity() {
+
+    }
+
+    @Override
+    public void getWeatherOfCityByYear() {
+
+    }
+}
