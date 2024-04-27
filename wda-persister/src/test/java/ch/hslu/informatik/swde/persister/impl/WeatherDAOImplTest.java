6c41cad02b28d2ee0875fb44a9da6f43b206cebe
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-27 02:57:52 +0200

    renamed files

--- wda-persister/src/test/java/ch/hslu/informatik/swde/persister/impl/WeatherDAOImplTest.java
+++ wda-persister/src/test/java/ch/hslu/informatik/swde/persister/impl/WeatherDAOImplTest.java
@@ -9,13 +9,12 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class WeatherDAOImplIT {
+public class WeatherDAOImplTest {
 
     @BeforeEach
     void setUp() {
