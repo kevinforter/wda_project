746036715a7ff9524c72ab67178362e61f272482
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-27 16:49:00 +0200

    added BusinessApi

---
+++ wda-business/src/main/java/ch/hslu/informatik/swde/business/BusinessAPI.java
@@ -0,0 +1,16 @@
+package ch.hslu.informatik.swde.business;
+
+public interface BusinessAPI {
+
+    void addAllCities();
+
+    void addCurrentWeatherOfCity();
+
+    void addWeatherOfCityByYear();
+
+    void getAllCities();
+
+    void getCurrentWeatherOfCity();
+
+    void getWeatherOfCityByYear();
+}
