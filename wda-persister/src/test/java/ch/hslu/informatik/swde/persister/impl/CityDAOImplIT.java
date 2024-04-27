39af7e6a9cc55eceb0ae5a245e5a51a3bee01d32
kevinforter <49223606+kevinforter@users.noreply.github.com>
2024-04-27 02:57:10 +0200

    added parameter tests

--- wda-persister/src/test/java/ch/hslu/informatik/swde/persister/impl/CityDAOImplIT.java
+++ wda-persister/src/test/java/ch/hslu/informatik/swde/persister/impl/CityDAOImplIT.java
@@ -3,13 +3,13 @@ package ch.hslu.informatik.swde.persister.impl;
 import ch.hslu.informatik.swde.domain.City;
 import ch.hslu.informatik.swde.persister.DAO.CityDAO;
 import ch.hslu.informatik.swde.persister.util.Util;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,46 +30,52 @@ public class CityDAOImplIT {
         Util.cleanDatabase();
     }
 
-    @Test
-    void test_SavingCity_ShouldBeSameAsFoundByID() {
+    @Tag("unittest")
+    @ParameterizedTest
+    @MethodSource("cityListProvider")
+    void test_SavingCity_ShouldBeSameAsFoundByID(List<City> listFromUtil) {
 
         CityDAO dao = new CityDAOImpl();
 
-        for (City c : Util.createCityList()) {
+        for (City c : listFromUtil) {
             dao.speichern(c);
             assertEquals(c, dao.findById(c.getId()), "Objekte stimmen nicht überein");
         }
 
     }
 
-    @Test
-    void test_SavingCity_ShouldBeSameAsFoundByName() {
+    @Tag("unittest")
+    @ParameterizedTest
+    @MethodSource("cityListProvider")
+    void test_SavingCity_ShouldBeSameAsFoundByName(List<City> listFromUtil) {
 
         CityDAO dao = new CityDAOImpl();
 
-        for (City c : Util.createCityList()) {
+        for (City c : listFromUtil) {
             dao.speichern(c);
             assertEquals(c, dao.findCityByName(c.getName()), "Objekte stimmen nicht überein");
         }
     }
 
-    @Test
-    void test_SavingCity_ShouldBeSameAsFoundByFieldNameAndValue() {
+    @Tag("unittest")
+    @ParameterizedTest
+    @MethodSource("cityListProvider")
+    void test_SavingCity_ShouldBeSameAsFoundByFieldNameAndValue(List<City> listFromUtil) {
 
         CityDAO dao = new CityDAOImpl();
 
-        for (City c : Util.createCityList()) {
+        for (City c : listFromUtil) {
             dao.speichern(c);
             assertEquals(c, dao.findEntityByField("name", c.getName()), "Objekte stimmen nicht überein");
         }
     }
 
-    @Test
-    void test_getAllCitiesFormDB_ShouldBeTheSameAsSavedList() {
+    @Tag("unittest")
+    @ParameterizedTest
+    @MethodSource("cityListProvider")
+    void test_getAllCitiesFormDB_ShouldBeTheSameAsSavedList(List<City> listFromUtil) {
 
         CityDAO dao = new CityDAOImpl();
-
-        List<City> listFromUtil = Util.createCityList();
         List<City> listFromDB;
 
         for (City c : listFromUtil) {
@@ -81,15 +87,25 @@ public class CityDAOImplIT {
         assertEquals(listFromUtil, listFromDB, "DB ist nicht kongruent zur Liste");
     }
 
-    @Test
-    void test_saveAllCities_ShouldMakeABulkSave() {
+    @Tag("unittest")
+    @ParameterizedTest
+    @MethodSource("cityMapProvider")
+    void test_saveAllCities_ShouldMakeABulkSave(LinkedHashMap<Integer, City> mapFromUtil) {
 
         CityDAO dao = new CityDAOImpl();
-
-        LinkedHashMap<Integer, City> mapFromUtil = Util.createCityMap();
 
         dao.saveAllCities(mapFromUtil);
         assertEquals(mapFromUtil.size(), dao.alle().size(), "Array not the Same");
 
+    }
+
+    static Stream<List<City>> cityListProvider() {
+        List<City> cities = Util.createCityList();
+        return Stream.of(cities);
+    }
+
+    static Stream<LinkedHashMap<Integer, City>> cityMapProvider() {
+        LinkedHashMap<Integer, City> cities = Util.createCityMap();
+        return Stream.of(cities);
     }
 }
